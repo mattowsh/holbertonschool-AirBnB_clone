c20dcbdb8b8cb79c4c6801dc285997e5e0b0f896
mattowsh <mattobelen@gmail.com>
2022-10-10 11:01:44 -0700

    task 3: updated_at attribute typing error was fixed; test id, created_at and updated_at were created

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel():
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """ Overrides the __str__ method so that it returns a personalizated
@@ -28,7 +28,7 @@ class BaseModel():
     def save(self):
         """ Updates the public instance attribute updated_at with the current
         datetime """
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ Returns a dictionary containing all keys/values of __dict__ of the
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Unittest for class BaseModel methods
+"""
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class Test_classBaseModel(unittest.TestCase):
+    """ Class that include all class BaseModel test """
+
+    def test_attr_types(self):
+        """ to check the types of the class attributes """
+        base0 = BaseModel()
+
+        self.assertEqual(type(base0.id), str)
+        self.assertEqual(type(base0.created_at), datetime)
+        self.assertEqual(type(base0.update_at), datetime)
