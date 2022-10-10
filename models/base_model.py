b98382a7eb1b0f52f52acc4c97a5c33969a9148c
mattowsh <mattobelen@gmail.com>
2022-10-10 12:38:34 -0700

    task 4 version 0

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,26 @@ from datetime import datetime
 class BaseModel():
     """ Defines all common attributes/methods for other classes """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Init constructor to generate new instances:
         ATTRIBUTES
             @id: uniq universal ID in str format
             @created_at: current datetime when an instance is created
             @update_at: current datetime when an instance is updated
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            dt_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.strptime(value, dt_format)
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(value, dt_format)
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ Overrides the __str__ method so that it returns a personalizated
