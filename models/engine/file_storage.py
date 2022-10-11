d2d4e012866ac4b2594e2a1c04ab83197fc335f7
mattowsh <mattobelen@gmail.com>
2022-10-11 11:25:03 -0700

    circular import was fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,24 +13,25 @@ class FileStorage():
 
     def all(self):
         """ Return the dict """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
-        self.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj
+        FileStorage.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path) """
         new_dict = {}
-        for key, value in self.__objects.items():
+        for key, value in FileStorage.__objects.items():
             new_dict[key] = value.to_dict()
-        with open(self.__file_path, 'w', encoding='utf-8') as f:
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
             # we pass the name of the dict and the name of the file(f)
             json.dump(new_dict, f)
-
+ 
     def reload(self):
         """ deserializes the JSON file to __objects """
-        if isfile(self.__file_path):
-            with open(self.__file_path, "r") as f:
-                # update __objects with the load file 
-                self.__objects.update(json.loads(f.read()))
+        if isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as f:
+                deserializated = json.load(f.read())
+                for key, value in desetializated.items():
+                    FileStorage.__objects[key] = value
