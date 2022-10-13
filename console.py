57e832d88396de6f8ccd9a92116b4960703d0520
mattowsh <mattobelen@gmail.com>
2022-10-13 11:32:23 -0700

    task 6: destroy method was created

--- console.py
+++ console.py
@@ -12,6 +12,7 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
     """ command line processor """
     prompt = '(hbnb)'
+    our_classes = [BaseModel]
 
     def do_quit(self, arg):
         """to exit the program"""
@@ -42,15 +43,14 @@ class HBNBCommand(cmd.Cmd):
         """ Prints the string representation of an instance """
         # to create a list of strings that contain the input
         prompt_args = args.split()
-
+        
         if len(prompt_args) == 0:
             print("** class name missing **")
             return
 
         elif len(prompt_args) == 1: 
             # if len(prompt_args) <= 2 means that input is incomplete,
-            our_classes = [BaseModel]
-            if (prompt_args[0] in our_classes) is False:
+            if (prompt_args[0] in self.our_classes) is False:
                 # check if ID is missing
                 print("** instance id missing **")
                 return
@@ -69,6 +69,38 @@ class HBNBCommand(cmd.Cmd):
                 print(objs[class_plus_id])
             except:
                 print("** no instance found **")
+
+    def do_destroy(self, args):
+        """ Deletes an instance based on the class name and id """
+        prompt_args = args.split()
+
+        if len(prompt_args) == 0:
+            # check if class name is missing
+            print("** class name missing **")
+            return
+
+        elif len(prompt_args) == 1:
+            if (prompt_args[0] in self.our_classes) is False:
+                # check if prompt_args[0] is a valid class name
+                print("** instance id missing **")
+                return
+            else:
+                # when prompt_args[0] is not a valid class name
+                print("** class doesn't exist **")
+                return
+
+        elif len(prompt_args) == 2:
+            try:
+                # string format: 'class.id'
+                class_delete = '.'.join(prompt_args)
+                objs = storage.all()
+                # update JSON file with the key 'class.id' removed
+                del objs[class_delete]
+                storage.save()
+            except:
+                print("** no instance found **")
+                return
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
