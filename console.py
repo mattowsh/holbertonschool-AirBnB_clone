780e2d06efaaee6cec61684f9fdfd6e5b15f7e66
mattowsh <mattobelen@gmail.com>
2022-10-15 09:56:35 -0700

    task 7 to_update was implemented, master branch

--- console.py
+++ console.py
@@ -6,8 +6,14 @@ command interpreter:
 import cmd
 import sys
 import uuid
-from models.base_model import BaseModel
 from models import storage
+from models.base_model import BaseModel
+#from models.user import User
+#from models.state import State
+#from models.city import City
+#from models.amenity import Amenity
+#from models.place import Place
+#from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -44,7 +50,7 @@ class HBNBCommand(cmd.Cmd):
         """ Prints the string representation of an instance """
         # to create a list of strings that contain the input
         prompt_args = args.split()
-        
+ 
         if len(prompt_args) == 0:
             print("** class name missing **")
             return
@@ -124,6 +130,50 @@ class HBNBCommand(cmd.Cmd):
             # if prompt_args[0] is a non-valid class name
             print("** class doesn't exist **")
             return
+
+    def do_update(self, args):
+        """ Updates an instance based on the class name and id by adding
+        or updating attribute"""
+        prompt_args = args.split()
+
+        if len(prompt_args) == 0:
+            # when input == update, and no more args
+            print("** class name missing **")
+            return
+
+        if (prompt_args[0] in self.our_classes) is True:
+            # check if the class name is a valid value
+            if len(prompt_args) < 2:
+                print("** instance id missing **")
+                return
+        else:
+            # when class name is not a valid value
+            print("** class doesn't exist **")
+            return
+
+        key = str(prompt_args[0]) + '.' + str(prompt_args[1])
+        objs = storage.all()
+
+        if (key in objs) is False:
+            # check if we have the key in our storage dict
+            print("** no instance found **")
+            return
+        elif len(prompt_args) < 3:
+            # in this case, the input only contain class name + id but
+            # is not contain an attr
+            print("** attribute name missing **")
+            return
+        elif len(prompt_args) < 4:
+            # in this case, the input only contain class name + id + attr
+            # but is not contain a value to be updated
+            print("** value missing **")
+            return
+
+        # after all those error "if"s, we can proceed with the class update
+        # ignoring all args [4:], in case of they exists
+        # we used eval to cast to the correct attribute type
+        objs[key].__dict__[prompt_args[2]] = eval(prompt_args[3])
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
